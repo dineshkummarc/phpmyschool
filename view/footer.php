c2235040e3dba806483c850c79a794978f2ad7f5
Dinesh Kumar <mapmydestination@gmail.com>
2024-08-11 21:41:32 +0530

    added versioning

--- view/footer.php
+++ view/footer.php
@@ -1,6 +1,6 @@
 	<footer class="main-footer">
         <div class="pull-right hidden-xs">
-          <b>Version</b> 2.3.0.1
+          <b>Version</b> 2.3.0.8
         </div>
         <strong>Copyright &copy; 2024 | Created by <a href="#">RHS Services</a>.</strong> All rights reserved.
      </footer>
