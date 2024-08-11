b87bb7608de095b345617b345daa2751f444c346
Dinesh Kumar <mapmydestination@gmail.com>
2024-08-11 21:07:56 +0530

    modified

--- controller/config.php
+++ controller/config.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username   = "root";
 $password   = "";
-$dbname     = "demosms";
+$dbname     = "phpmyschool";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
